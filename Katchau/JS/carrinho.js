df8563e77a48d8715e82565febe9e379e15695d3
KayanDenizo <kayan.denizo@gmail.com>
2024-10-16 21:44:03 -0300

    Adicionando funcao de subtotal, quantidade e preço

--- Katchau/JS/carrinho.js
+++ Katchau/JS/carrinho.js
@@ -9,7 +9,13 @@ const botaoCarrinho = document.getElementById('botaoCarrinho');
 // Selecione a sidebar
 const sidebar = document.getElementById('sidebar');
 
+// Footer Sidebar
 const footerSidebar = document.getElementById('footerSidebar');
+
+// Subtotal
+let subtotal = 0;
+
+let quantidadeProduto = 0;
 
 // Select All
 const selectAll = document.getElementById('selectAll');
@@ -36,7 +42,7 @@ botoesAdicionar.forEach((botao) => {
         sidebar.classList.add('show');
         body.classList.add('bg');
         if (listaProdutos.children.length >= 0) {
-            footerSidebar.style.display='block';
+            footerSidebar.style.display = 'block';
         }
         // Exiba a sidebar
 
@@ -44,7 +50,7 @@ botoesAdicionar.forEach((botao) => {
         const produto = {
             nome: botao.parentNode.querySelector('.title').textContent,
             descricao: botao.parentNode.querySelector('.off').textContent,
-            preco: botao.parentNode.querySelector('.price').textContent,
+            preco: parseFloat(botao.parentNode.querySelector('.price').textContent.replace('R$ ', '')),
             foto: botao.parentNode.querySelector('.imagem-product').src,
             quantidade: 1
         };
@@ -58,7 +64,7 @@ botoesAdicionar.forEach((botao) => {
             <img src="${produto.foto}" alt="${produto.nome}" id="foto-produto">
             <div class="info-produto">
                 <p>${produto.nome}</p>
-                <p>${produto.preco}</p>
+                <p>R$ ${produto.preco},00</p>
                 <div class="bottom-container">
                     <div class="quantidade">
                         <button class="diminuir">-</button>
@@ -78,11 +84,25 @@ botoesAdicionar.forEach((botao) => {
 
         listaProdutos.appendChild(itemProduto);
 
+        subtotal += produto.preco;
+        document.getElementById('subTotal').innerHTML = `R$ ${subtotal.toFixed(2)}`;
+
+        quantidadeProduto += produto.quantidade;
+        document.getElementById('quantidade_produtos').innerHTML = `${quantidadeProduto} produtos`;
+        document.getElementById('preco_total').textContent = `R$ ${subtotal.toFixed(2)}`
+
         // Adicione um evento de clique ao botão de aumentar quantidade
         itemProduto.querySelector('.aumentar').addEventListener('click', () => {
             const quantidadeTexto = itemProduto.querySelector('.quantidade-texto');
             const quantidade = parseInt(quantidadeTexto.textContent) + 1;
             quantidadeTexto.textContent = quantidade;
+
+            subtotal += produto.preco;
+            document.getElementById('subTotal').textContent = `R$ ${subtotal.toFixed(2)}`;
+            document.getElementById('preco_total').textContent = `R$ ${subtotal.toFixed(2)}`
+
+            quantidadeProduto += produto.quantidade;
+            document.getElementById('quantidade_produtos').innerHTML = `${quantidadeProduto} produtos`;
         });
 
         // Adicione um evento de clique ao botão de diminuir quantidade
@@ -91,19 +111,72 @@ botoesAdicionar.forEach((botao) => {
             const quantidade = parseInt(quantidadeTexto.textContent) - 1;
             if (quantidade >= 1) {
                 quantidadeTexto.textContent = quantidade;
+                subtotal -= produto.preco; // Subtrai o preço do produto do subtotal
+                document.getElementById('subTotal').textContent = `R$ ${subtotal.toFixed(2)}`;
+                document.getElementById('preco_total').textContent = `R$ ${subtotal.toFixed(2)}`
+
+                quantidadeProduto -= produto.quantidade;
+                document.getElementById('quantidade_produtos').innerHTML = `${quantidadeProduto} produtos`;
             }
+
         });
 
         // Adicione um evento de clique ao botão de excluir
-        itemProduto.querySelector('.excluir').addEventListener('click', () => {
+        // Adicione um evento de clique ao botão de excluir
+        itemProduto.querySelector('.excluir').addEventListener('click', (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+
+            // Obtenha o valor total do produto que foi removido antes de removê-lo do DOM
+            const quantidade = parseInt(itemProduto.querySelector('.quantidade-texto').textContent);
+            const precoProduto = parseFloat(itemProduto.querySelector('.info-produto p:nth-child(2)').textContent.replace('R$ ', '').replace(',', '.'));
+
+            // Subtraia o valor do produto do subtotal
+            subtotal -= (precoProduto * quantidade);
+            document.getElementById('subTotal').textContent = `R$ ${subtotal.toFixed(2)}`;
+            document.getElementById('preco_total').textContent = `R$ ${subtotal.toFixed(2)}`
+
+            quantidadeProduto -= quantidade;
+            document.getElementById('quantidade_produtos').innerHTML = `${quantidadeProduto} produtos`;
+
+            // Remova o item do carrinho
             listaProdutos.removeChild(itemProduto);
+
+            // Verifique se a lista de produtos está vazia e esconda o footer se necessário
             if (listaProdutos.children.length === 0) {
-                footerSidebar.style.display='none';
+                footerSidebar.style.display = 'none';
             }
+
+            // Exibir a sidebar se necessário
+            sidebar.classList.add('show');
         });
 
-        itemProduto.querySelector('.arquivar').addEventListener('click', () => {
+        itemProduto.querySelector('.arquivar').addEventListener('click', (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+
+            // Obtenha o valor total do produto que foi removido antes de removê-lo do DOM
+            const quantidade = parseInt(itemProduto.querySelector('.quantidade-texto').textContent);
+            const precoProduto = parseFloat(itemProduto.querySelector('.info-produto p:nth-child(2)').textContent.replace('R$ ', '').replace(',', '.'));
+
+            // Subtraia o valor do produto do subtotal
+            subtotal -= (precoProduto * quantidade);
+            document.getElementById('subTotal').textContent = `R$ ${subtotal.toFixed(2)}`;
+            document.getElementById('preco_total').textContent = `R$ ${subtotal.toFixed(2)}`
+
+            quantidadeProduto -= quantidade;
+            document.getElementById('quantidade_produtos').innerHTML = `${quantidadeProduto} produtos`;
+
+            // Remova o item do carrinho
             listaProdutos.removeChild(itemProduto);
+
+            // Verifique se a lista de produtos está vazia e esconda o footer se necessário
+            if (listaProdutos.children.length === 0) {
+                footerSidebar.style.display = 'none';
+            }
+
+            // Exibir a sidebar se necessário
+            sidebar.classList.add('show');
         });
     });
 });
