954068389d6fd3887a0c25aa28b46c58aba75a98
KayanDenizo <kayan.denizo@gmail.com>
2024-11-05 18:16:57 -0300

    Avançando sistema de cadastro e login katchau

--- teste_de_sistema_de_cadastro_clientes/app.py
+++ teste_de_sistema_de_cadastro_clientes/app.py
@@ -7,18 +7,18 @@ app.secret_key = 'chave_secreta_para_sessoes'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db= SQLAlchemy(app)
+db = SQLAlchemy(app)
 
-class User (db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(150), unique = True, nullable = False)
-    email = db.Column(db.String(150), unique = True, nullable = False)
-    password = db.Column(db.String(150), nullable = False)
-    
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
 with app.app_context():
     db.create_all()
 
-@app_route('/register', methods=['GET', 'POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         username = request.form['username']
@@ -36,47 +36,37 @@ def register():
         db.session.commit()
         
         flash('Cadastro realizado com sucesso! Agora você pode fazer login.')
-        return
-    redirect(url_for('login'))
+        return redirect(url_for('login'))
     
-    return
-render_template('register.html')
+    return render_template('register.html')
 
-# Rota de login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
         
-        #Busca o usuário pelo email
         user = User.query.filter_by(email=email).first()
 
-        # Verifica a senha
         if user and check_password_hash(user.password, password):
             session['user_id'] = user.id
-            flash('logi realizado com sucesso!')
-            return
-            redirect(url_for('dashboard'))
+            flash('Login realizado com sucesso!')
+            return redirect(url_for('dashboard'))
         else:
             flash('Email ou senha incorretos!')
-    return
-render_template('login.html')
+    
+    return render_template('login.html')
 
-# Rota do painel do usuário (apenas logado)
 @app.route('/dashboard')
 def dashboard():
     if 'user_id' not in session:
         flash('Faça login para acessar o painel')
-        return
-        redirect(url_for('login'))
+        return redirect(url_for('login'))
         
-        return "Bem vindo ao painel do usuario!"
+    return "Bem vindo ao painel do usuario!"
 
-# Rota de logout
 @app.route('/logout')
 def logout():
     session.pop('user_id', None)
     flash('Você foi desconectado com sucesso!')
-    return
-redirect(url_for('login'))
+    return redirect(url_for('login'))
